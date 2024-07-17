0e362c03e7e1ba45fa96693a757932f3df717866
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-17 23:24:40 +0530

    string

---
+++ string2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class string2
+{
+    public static void main(String args[])
+    {
+        long start = System.currentTimeMillis();
+        StringBuffer buffer =new StringBuffer("Hello");
+        System.out.println(buffer.capacity());
+        for(int i=0;i<1000;i++)
+        {
+            buffer.append("World");
+        }
+        System.out.println("Time taken by string buffer:"+(System.currentTimeMillis())- start);
+    }
+}
