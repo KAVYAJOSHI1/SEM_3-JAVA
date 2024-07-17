80faeb17efee00460dd21677d79078073bd52ee6
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-17 17:12:58 +0530

    a

---
+++ conditional.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class conditional
+{
+    public static void main(String args[])
+    {
+        boolean a=true;
+        String b=(a)?"true":"false";
+        System.out.println(b);
+}
+}
