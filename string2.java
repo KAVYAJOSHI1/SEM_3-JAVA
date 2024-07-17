f1509bd70fb3adde8e5924ba399fb9751022949b
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-17 23:27:02 +0530

    a

--- string2.java
+++ string2.java
@@ -1,15 +1,13 @@
 import java.util.*;
-public class string2
-{
-    public static void main(String args[])
-    {
+
+public class string2 {
+    public static void main(String args[]) {
         long start = System.currentTimeMillis();
-        StringBuffer buffer =new StringBuffer("Hello");
-        System.out.println(buffer.capacity());
-        for(int i=0;i<1000;i++)
-        {
+        StringBuffer buffer = new StringBuffer("Hello");
+        System.out.println("Initial capacity: " + buffer.capacity());
+        for (int i = 0; i < 1000; i++) {
             buffer.append("World");
         }
-        System.out.println("Time taken by string buffer:"+(System.currentTimeMillis())- start);
+        System.out.println("Time taken by string buffer: " + (System.currentTimeMillis() - start) + " ms");
     }
 }
