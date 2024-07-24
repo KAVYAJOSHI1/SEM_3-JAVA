b964e9af0d214f8de069e707ac399f186627d3ee
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-24 14:28:18 +0530

    a

--- TASK3/1).java
+++
@@ -1,2 +0,0 @@
-//1. Write a Java program to demonstrate constructor overloading by creating multiple constructors
-//for a class Person that accept different parameters (e.g., name, age, address)
---
+++ TASK3/a1.java
@@ -0,0 +1,34 @@
+//1. Write a Java program to demonstrate constructor overloading by creating multiple constructors
+//for a class Person that accept different parameters (e.g., name, age, address)
+
+import java.util.*;
+public class a1
+{
+    a1(String name,int age,String address)
+    {
+        System.out.println(name+" "+age+" "+address);
+    }
+
+    a1(String name,int age)
+    {
+        System.out.println(name+" "+age);
+    }
+
+    a1(String name)
+    {
+        System.out.println(name);
+    }
+    
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("ENTER THE NAME,AGE AND ADDRESS");
+        String name=s.next();
+        int age=s.nextInt();
+        String address=s.next();
+        a1 obj=new a1(name,age,address);
+        a1 obj1=new a1(name,age);
+        a1 obj2=new a1(name);
+
+    }
+}
