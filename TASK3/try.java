b15235ced702e587304ceead2a27d74efb5c595d
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-09 15:13:25 +0530

    a

---
+++ TASK3/try.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class try
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter the two numbers");
+        try{
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int c=a/b;
+        System.out.println("SUM IS:"+c);
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("INVALID DIVISION");
+        }
+        finally{
+            s.close();
+        }
+
+    }
+}
