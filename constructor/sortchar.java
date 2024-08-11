b93f351e7f44fba234012a1fa7584d4cd991e8e7
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-11 14:31:43 +0530

    a

--- constructor/sortchar.java
+++ constructor/sortchar.java
@@ -10,27 +10,47 @@ public class sortchar
         this.marks=b;
         this.Grade=c;
     }
+    void display()
+    {
+        System.out.println("NAME IS :"+name);
+        System.out.println("MARKS ARE:"+marks);
+        System.out.println("GRADE IS :"+Grade);
+    }
 
 
-}
+
 public static void main(String args[])
 {
     String name;
     int marks;
     char Grade;
     Scanner s=new Scanner(System.in);
-    sortchar[] obj=new sortchar[5];
-    for(int i=0;i<5;i++)
+    sortchar[] obj=new sortchar[2];
+    for(int i=0;i<2;i++)
     {
         System.out.println("ENTER THE NAME:");
         name=s.nextLine();
         System.out.println("ENTER THE MARKS:");
         marks=s.nextInt();
         System.out.println("ENTER THE GRADE:");
-        Grade=s.nextCharAt(0);
-        obj[i]=new obj(name,marks,Grade);
+        Grade=s.next().charAt(0);
+        s.nextLine();
+        obj[i]=new sortchar(name,marks,Grade);
 
 
     }
+    Arrays.sort(obj, new Comparator<sortchar>()
+    {
+      public int compare(sortchar a,sortchar b)
+      {
+        return a.Grade-b.Grade;
+      }
+    }
+    );
+    for(int i=0;i<2;i++)
+    {
+        obj[i].display();
+    }
 
+}
 }
