58c477354358ddc0b4044246e30447a94bbbf163
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-14 14:33:45 +0530

    a

---
+++ TASK4/truck.java
@@ -0,0 +1,22 @@
+public class truck extends vehicle
+{
+    String name;
+    int price;
+    truck(int a,int b,String c,int d)
+    {
+      super(a,b);
+      name=c;
+      price=d;
+    }
+    void display()
+    {
+        super.display();
+        System.out.println("NAME IS:"+ name);
+        System.out.println("PRICE IS:"+price);
+    }
+    public static void main(String args[])
+    {
+        truck obj=new truck(50,40,"MARCOPOLO",6598665);
+        obj.display();
+    }
+}
