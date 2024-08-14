dcdc30cd09d696f41e33da049502ad967592a75c
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-14 10:13:10 +0530

    a

--- TASK3/a10.java
+++ TASK3/a10.java
@@ -1,15 +1,13 @@
 /*10. Write a Java program to handle an exception without using try-catch by declaring a method that
 throws an exception (e.g., IOException) and invoking this method in the main method.   */
 import java.io.IOException;
-
-import java.io.IOException;
-
 public class a10 {
-   public static void main(String[] args) {
-        methodThatThrowsException();
+    public static void main(String[] args) {
+        try {
+            methodThatThrowsException();
+        } catch (IOException e) {
+            System.out.println("IOException occurred: " + e.getMessage());
+        }
     }
-
-    public static void methodThatThrowsException() throws IOException {
-        throw new IOException("This is an exception");
-    }
-}
+    public static void methodThatThrowsException() throws IOException { throw new IOException("This is an exception"); }
+}        
