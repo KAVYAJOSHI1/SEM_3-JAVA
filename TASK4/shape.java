b50e931d0bd5da363c73dd58b7ab21a56bab9e08
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-14 14:41:02 +0530

    A

---
+++ TASK4/shape.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class shape
+{
+    int a;
+    int b;
+    shape(int l,int B)
+    {
+        a=l;
+        b=B;
+    }
+    void area()
+    {
+        return a*b;
+    }
+   
+
+}
