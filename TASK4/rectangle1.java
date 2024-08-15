7fa9198cafbb579290d7ace352c7b25501068166
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-15 16:56:28 +0530

    a

---
+++ TASK4/rectangle1.java
@@ -0,0 +1,11 @@
+public class rectangle1 extends shape1
+{
+    void draw()
+    {
+        System.out.println("DRAWN RECTANGLE");
+    }
+    public static void main(String args[])
+    {
+      draw();
+    }
+} 
