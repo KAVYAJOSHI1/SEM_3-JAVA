77544db4c0ee63c8ff5bb3063e8f0c5c59781430
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-15 16:37:41 +0530

    a

---
+++ TASK4/Animal.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Animal
+{
+    String sound;
+    Animal(Sting a)
+    {
+        sound=a;
+    }
+    void makesound()
+    {
+        System.out.println("ANIMAL MAKES SOUND"+sound);    }
+}
