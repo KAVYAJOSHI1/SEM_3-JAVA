a86cfb4babafc6ba2eebe9a0c92ddc2838f5be96
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-15 17:09:04 +0530

    a

---
+++ TASK4/child.java
@@ -0,0 +1,13 @@
+public class child extends parent
+{
+    void display()
+    {
+        super.display();
+
+    }
+    public static void main(String args[])
+    {
+        child obj= new child();
+        obj.display();
+    }
+}
