c052a3b4d759a299b4474064158cabc79a080ca2
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-09-11 17:56:16 +0530

    A

---
+++ TASK6/colors.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class colors
+{
+    public static void main(String args[])
+    {
+        ArrayList<String> list =new ArrayList<>();
+        list.add("RED");
+        list.add("GREEN");
+        list.add("BLUE");
+        System.out.println(list);
+
+        list.add(1,"BROWN");
+        System.out.println(list);
+    }
+}
